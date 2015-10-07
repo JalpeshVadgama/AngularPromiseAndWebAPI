a9f41bb12d256182a2078ad5213f77f8a064d1df
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-08 00:42:31 +0530

    Added API Controller for Employee Web API

---
+++ AngularJSPromise/AngularJSPromise/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using AngularJSPromise.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace AngularJSPromise.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private readonly List<Employee> _employees;
+
+        public EmployeesController()
+        {
+            _employees = new List<Employee>()
+            {
+                new Employee {Id = 1, FirstName = "Jalpesh", LastName = "Vadgama"},
+                new Employee{Id=2, FirstName = "Vishal",LastName = "Vadgama"}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllProducts()
+        {
+            return _employees;
+        }
+    }
+}
