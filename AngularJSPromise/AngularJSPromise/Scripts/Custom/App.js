0a19a05e725a6cba188b9e2b69d330d5cd0f22f3
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-08 01:01:07 +0530

    Added Angular js and app.js

---
+++ AngularJSPromise/AngularJSPromise/Scripts/Custom/App.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../angular.min.js" />
+
+//creating angular module
+var app = angular.module("promiseApp", "[]");
+
+app.controller("employeesCtrl", function($scope,employeeService) {
+
+});
+
+app.factory("EmployeeService",function() {
+    var promise = $http.get("/api/employees");
+    
+});
+
